bb0abe8f864639cb6b4399cdbd328d5b4e0b6e07
prothbard <pedrotec@protonmail.com>
2020-05-14 23:58:49 -0300

    ajustando layout

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+  /*PODE SER SÓ ALERT*/
+  //window.alert('PRIMEIRA MENSAGEM')
+  //window.confirm('Curtindo a experiencia?')
+
+  var nome = window.prompt('Qual é seu nome ?')
+
+  alert('bem vindo ' + nome + '!')
